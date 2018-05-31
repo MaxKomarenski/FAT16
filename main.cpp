c67feb52f25c722979da1e2dc9c793c531ef8c1a
Bohdan Shtohrinets <shtohrinets@ucu.edu.ua>
2018-05-31 11:22:10 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <sstream>
+#include <cstring>
+#include <map>
+
+struct boot {
+    std::pair<long, long> text_identifier_OS = std::make_pair(0, 3);
+    std::pair<long, long> machine_instruction = std::make_pair(3, 8);
+    std::pair<long, long> BytesPerSector = std::make_pair(11, 2);//512
+    std::pair<long, long> SectorPerCluster = std::make_pair(13, 1);
+    std::pair<long, long> ReservedSector = std::make_pair(14, 2);
+    std::pair<long, long> NumberOfFAts = std::make_pair(16, 1);
+    std::pair<long, long> RootEntries = std::make_pair(17, 2);
+    std::pair<long, long> MediaDescriptor = std::make_pair(21, 1);
+    std::pair<long, long> SectorPerFat = std::make_pair(22, 2);
+    std::pair<long, long> SectorPerTrack = std::make_pair(24, 2);
+    std::pair<long, long> Heads = std::make_pair(26, 2);
+    std::pair<long, long> HiddenSectors = std::make_pair(28, 4);
+    std::pair<long, long> BigTotalSectors = std::make_pair(32, 4);
+    std::pair<long, long> PhysicalDiscNumbers = std::make_pair(36, 1);
+    std::pair<long, long> CurrentHead = std::make_pair(37, 1);
+    std::pair<long, long> Signature = std::make_pair(38, 1);
+    std::pair<long, long> VolumeSerialNumber = std::make_pair(39, 4);
+    std::pair<long, long> VolumeLabel = std::make_pair(43, 11);
+    std::pair<long, long> SystemID = std::make_pair(54, 8);
+    std::pair<long, long> LoaderCodeArea = std::make_pair(62, 448);
+    std::pair<long, long> BootSignature = std::make_pair(510, 2);
+};
 
 std::vector<uint8_t> read_image_of_fat16(const std::string &name_of_file){
     std::vector<uint8_t> bytes;
@@ -10,14 +37,55 @@ std::vector<uint8_t> read_image_of_fat16(const std::string &name_of_file){
     file.seekg(0, std::ios::beg); // return pointer to begin of file.
     bytes.resize(size + 1); // set size of vector
     file.read(reinterpret_cast<char *>(bytes.data()), size); // write bytes to vector
+
     return bytes;
 }
 
+std::vector<uint8_t> slice(std::vector<uint8_t> &vector, int m, int n) {
+    auto first = vector.cbegin() + m;
+    auto last = vector.cbegin() + n + 1;
+
+    std::vector<uint8_t> vec(first,last);
+    return vec;
+}
+
+std::vector<uint8_t> get_bytes_per_sector(std::vector<uint8_t> &bytes, std::pair<int, int>byteRange){
+    std::vector<uint8_t> sub_vector = slice(bytes, byteRange.first, byteRange.second);
+    return sub_vector;
+}
 
 
 int main() {
+    std::map<std::string, std::pair<int, int>> bootOptions = {{"text_identifier_OS", std::pair<int, int>(0,2)},
+                                                            {"machine_instruction", std::pair<int, int>(3,10)},
+                                                                    {"bytes per sector", std::pair<int, int>(11,12)},
+                    {"sectors per cluster", std::pair<int, int>(13,13)},{"size of reserved area", std::pair<int, int>(14,15)},
+                            {"number of FATs", std::pair<int, int>(16,16)},{"max number of files in root directory", std::pair<int, int>(17,18)},
+                                    {"number of sectors in the file system", std::pair<int, int>(19,20)},{"media type", std::pair<int, int>(21,21)},
+                                                              {"size of each FAT", std::pair<int, int>(22,23)},{"sectors per track in storage device", std::pair<int, int>(24,25)},
+                                                                      {"number of heads in storage device", std::pair<int, int>(26,27)}, {"number of sectors before the start partition", std::pair<int, int>(28,31)},
+                                                              {"number of sectors in the file system", std::pair<int, int>(32,35)}, {"signature value", std::pair<int, int>(510,511)}};
+
+    size_t sectorSize = 512;
     std::cout << "work" << std::endl;
-    std::vector<uint8_t> bytes = read_image_of_fat16("hd0_just_FAT16_without_MBR.img");
-    
+    std::vector<uint8_t> bytes = read_image_of_fat16("../hd0_just_FAT16_without_MBR.img");
+
+    std::cout<<"Sector size: " << sectorSize << std::endl;
+    std::cout<<"Sectors per cluster: "<< (bytes.size() - sectorSize*4)/sectorSize << std::endl;
+    std::cout<<"Number of FATs: " << 2 << std::endl;
+    std::cout<<"Number of FATs copies sectors/bytes:" << sectorSize*2 <<std::endl;
+    std::cout<<"Root size:" << std::endl;
+    std::cout<<"Reserved sectors:" << std::endl;
+
+    /*for(int i=0 ; i<512; ++i){
+        std::cout<<bytes[i];
+    }*/
+
+    std::pair<int, int> ranges(16,16);
+
+    for(int i=0; i < get_bytes_per_sector(bytes, ranges).size(); ++i){
+        std::cout<<unsigned(get_bytes_per_sector(bytes, ranges)[i])<<std::endl;
+    }
+
     return 0;
 }
